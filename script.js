faf552786acca57889d1ea8f75202ffb4c8ec1f7
realJimboSlice <jimm7493@stud.kea.dk>
2024-02-01 15:57:00 +0100

    Missed the part of the assignment where it said to add the moms back to the beloeb, so redid the calculation

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function momsBeregner(beloeb, moms = 25) {
   let momsIProcent = moms / 100;
   let result = beloeb * momsIProcent;
-  return result;
+  let sum = beloeb + result;
+  return sum;
 }
 
 document
@@ -9,14 +10,16 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let beloeb = document.getElementById("beloeb").value;
+    let beloeb = parseFloat(document.getElementById("beloeb").value);
     let moms = document.getElementById("moms").value;
     if (moms === "") {
       moms = undefined;
+    } else {
+      moms = parseFloat(moms);
     }
 
     let momsResult = momsBeregner(beloeb, moms);
     console.log("DKK", momsResult);
     let resultElement = document.getElementById("result");
-    resultElement.innerHTML = `The calculated moms is: <strong>DKK ${momsResult}</strong>`;
+    resultElement.innerHTML = `The calculated beloeb + moms is: <strong>DKK ${momsResult}</strong>`;
   });
